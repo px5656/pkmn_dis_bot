ffccf6d05271c7bc4ff9ba785d47325960dc2768
px5656 <px5656@gmail.com>
2021-02-22 22:35:22 +0000

    check feature, method implemented, dynamic to static team, SQL statement

--- pkmn_record_bot.py
+++ pkmn_record_bot.py
@@ -1,5 +1,5 @@
 #pkmn_record_bot.py
-#tl;dr – connects to db and can do a simple command
+#tl;dr – bit messy but can connect to a db and write to a db with simple commands!
 
 
 
@@ -37,9 +37,16 @@ async def on_ready(): #this is a predefined function name, can't change this!!
 
 #create a method to ask qursitons neater
 async def ask_question(ctx, question):
+
+    #check that the reply was from op and is in the same channel so that the parameters passed are correct!
+    def check(m):
+        return m.author == ctx.message.author and m.channel == ctx.message.channel
+
+
     await ctx.send(question)
-    answer = await bot.wait_for("message", timeout=60)
+    answer = await bot.wait_for('message', check=check, timeout=60)
     return answer.content
+
 
 
 #add a new record to the db
@@ -47,28 +54,50 @@ async def ask_question(ctx, question):
 async def create(ctx):
     user_id = ctx.message.author.id #get the user's id via ctx
 
-    await ctx.send("What game are you adding?")
-    game_title = await bot.wait_for("message", timeout=60)
-
-
-    await ctx.send("How many pokemon are on your team?")
-    team_amount = await bot.wait_for("message", timeout=60)
-
+    game_title = await ask_question(ctx, "What game is the pokemon team from?")
 
     pkmn_team = []
-    for i in range(int(team_amount.content)):
+    for i in range(6):
         await ctx.send(f"Please add pokemon {str(i + 1)} on your team: ")
-        pkmn = await bot.wait_for("message", timeout=60)
+        pkmn = await bot.wait_for('message', timeout=60)
         pkmn_team.append(pkmn.content)
 
-    # if team_amount == 0:
-    #     await ctx.send("You need to have at least one pokemon! :O")
-    # elif team_amount > 6:
-    #     await ctx.send("That's too many pokemon! You can have between 1 and 6 pokemon on your team.")
-
-    await ctx.send(f"You have added {game_title.content} with {pkmn_team}.") #for testing
+    await ctx.send(f"You have added {game_title} with {pkmn_team}.") #for testing
 
 
+    # #after getting the vars, put them into an SQL statement
+    sql_statement = f""" INSERT INTO user
+                         ('user_id',
+                          'game_title',
+                          'pkmn_1',
+                          'pkmn_2',
+                          'pkmn_3',
+                          'pkmn_4',
+                          'pkmn_5',
+                          'pkmn_6')
+                         VALUES('{str(user_id)}',
+                                '{game_title}',
+                                '{pkmn_team[0]}',
+                                '{pkmn_team[1]}',
+                                '{pkmn_team[2]}',
+                                '{pkmn_team[3]}',
+                                '{pkmn_team[4]}',
+                                '{pkmn_team[5]}');
+                     """
+
+    #cur is a SQL word, so u cant change the name :'v
+    cur = sql_conn.cursor()
+    cur.execute(sql_statement)
+    sql_conn.commit()
+
+
+#connect to the bot via the secret token and bot ahoy!
+bot.run(bot_token)
+
+
+
+
+## EXTRA STUFF ##
 
 # @bot.command()
 # async def add(ctx, game_title):
@@ -80,5 +109,19 @@ async def create(ctx):
 #     var = await bot.wait_for('message', timeout=time to wait)
 
 
+###if statement
+# if team_amount == 0:
+#     await ctx.send("You need to have at least one pokemon! :O")
+# elif team_amount > 6:
+#     await ctx.send("That's too many pokemon! You can have between 1 and 6 pokemon on your team.")
 
-bot.run(bot_token)
+#dynamic team amount
+# pkmn_team = []
+# for i in range(int(team_amount.content)):
+#     await ctx.send(f"Please add pokemon {str(i + 1)} on your team: ")
+#     pkmn = await bot.wait_for("message", timeout=60)
+#     pkmn_team.append(pkmn.content)
+
+
+#     await ctx.send("What game are you adding?")
+#     game_title = await bot.wait_for('message', timeout=60)
